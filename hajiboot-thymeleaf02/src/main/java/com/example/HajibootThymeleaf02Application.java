5cd98deea74c3d0a0de2df19aebfe4d3c1d2cab6
takuro-study <35710969+takuro-study@users.noreply.github.com>
2018-01-23 12:41:07 +0900

    test

---
+++ hajiboot-thymeleaf02/src/main/java/com/example/HajibootThymeleaf02Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HajibootThymeleaf02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HajibootThymeleaf02Application.class, args);
+	}
+}
---
+++ hajiboot-thymeleaf02/src/main/java/com/example/web/CustomerRestController.java
@@ -0,0 +1,45 @@
+package com.example.web;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.example.domain.Customer;
+import com.example.service.CustomerService;
+
+@Controller
+@RequestMapping("api/customers")
+public class CustomerRestController {
+
+	@Autowired
+	CustomerService customerService;
+
+	@GetMapping
+	String list(Model model) {
+		List<Customer> customers = customerService.findAll();
+		model.addAttribute("customers", customers);
+		return "customers/list";
+	}
+	
+}
+
